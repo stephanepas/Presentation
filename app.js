af328d0c0642a94770ecd217e375a2405f5c0ff2
stephanepas <stephane.pastor88@free.fr>
2021-12-30 16:03:50 +0100

    thrid commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const navbar = document.querySelector('.navbar');
+const ham = document.querySelector('.ham');
+const curseur = document.querySelector('.curseur');
+const menuLinks = document.querySelectorAll('.menuLink')
+
+// Animation curseur
+document.addEventListener('mousemove', e => {
+    curseur.style.top = e.pageY -20 + 'px';
+    curseur.style.left = e.pageX -20 + 'px';
+})
+document.addEventListener('click', ()=>{
+    curseur.classList.add('expand');
+    setTimeout(() => {
+        curseur.classList.remove('expand');
+    }, 500);
+})
+
+// bascule le menu hamburger in and out quand on click dessus
+function toggleHamburger(){
+  navbar.classList.toggle("showNav")
+  ham.classList.toggle("showClose")
+}
+
+ham.addEventListener("click", toggleHamburger)
+
+// bascule quand on click sur les liens
+menuLinks.forEach( 
+  function(menuLink) { 
+    menuLink.addEventListener("click", toggleHamburger) 
+  }
+)
