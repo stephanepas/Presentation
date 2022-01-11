6b9c76175a2c3f92089744ed475d7a3419e3b461
stephanepas <stephane.pastor88@free.fr>
2022-01-11 23:53:47 +0100

    commit

--- app.js
+++ app.js
@@ -5,89 +5,83 @@ const menuLinks = document.querySelectorAll('.menuLink');
 const diapos = document.querySelectorAll('.diapo');
 
 window.onload = () => {
-    const transition_el = document.querySelector('.transition');
-    const anchors = document.querySelectorAll('.menuLink');
+  const transition_el = document.querySelector('.transition');
+  // constante pour afficher titre de la page en version mobile
+  const largeurFenetre = window.innerWidth;
+  const titrePage = document.title;
 
-console.log(transition_el)
-console.log(anchors)
-    setTimeout(() => {
-      transition_el.classList.remove('is-active');
-    }, 500);
-  
-    for (let j = 0; j < anchors.length; j++) {
-  
-      anchors[j].addEventListener('click', e => {
+  setTimeout(() => {
+    transition_el.classList.remove('is-active');
+  }, 300);
+
+  for (let j = 0; j < menuLinks.length; j++) {
+    menuLinks[j].addEventListener('click', e => {
+      if (largeurFenetre >= 1100) {
         e.preventDefault();
         let target = e.target.href;
-  
-        console.log(transition_el);
-  
         transition_el.classList.add('is-active');
-  
-        console.log(transition_el);
-  
         setInterval(() => {
           window.location.href = target;
-        }, 500);
-      })
+        }, 300);
+      }
+    })
+  }
+
+  ajoutDivMobile()
+  // fonction pour ajouter le titre de la page
+  function ajoutDivMobile() {
+    if (largeurFenetre < 1100 && titrePage != "Présentation de mon projet") {
+      const parentDivAjoute = document.querySelector('.container');
+      const divAjoute = document.createElement('div');
+
+      divAjoute.className = 'divTitreMobile';
+      divAjoute.innerHTML = titrePage;
+      parentDivAjoute.parentNode.insertBefore(divAjoute, parentDivAjoute);
     }
   }
+}
 
 
 // position curseur
 document.addEventListener('mousemove', e => {
-    curseur.style.top = e.pageY -20 + 'px';
-    curseur.style.left = e.pageX -20 + 'px';
-    curseur.classList.add('cursor');
+  curseur.style.top = e.pageY - 20 + 'px';
+  curseur.style.left = e.pageX - 20 + 'px';
+  curseur.classList.add('cursor');
 })
 // Animation curseur
-document.addEventListener('click', ()=>{
-    curseur.classList.add('expand');
-    setTimeout(() => {
-        curseur.classList.remove('expand');
-    }, 500);
+document.addEventListener('click', () => {
+  curseur.classList.add('expand');
+  setTimeout(() => {
+    curseur.classList.remove('expand');
+  }, 500);
 })
 
 // bascule le menu hamburger in and out quand on click dessus
-function toggleHamburger(){
-    navbar.classList.toggle("showNav")
-    ham.classList.toggle("showClose");
+function toggleHamburger() {
+  navbar.classList.toggle("showNav")
+  ham.classList.toggle("showClose");
 }
 
 ham.addEventListener("click", toggleHamburger)
 
 // bascule quand on click sur les liens
-menuLinks.forEach( 
-  function(menuLink) { 
+menuLinks.forEach(
+  function (menuLink) {
     menuLink.addEventListener("click", toggleHamburger)
   }
 )
 
 diapos.forEach(diapo => {
-    diapo.addEventListener('click', () => {
-        removeActiveClasses()
-        diapo.classList.add('isActive')
-    })
+  diapo.addEventListener('click', () => {
+    removeActiveClasses()
+    diapo.classList.add('isActive')
+  })
 })
 function removeActiveClasses() {
-    diapos.forEach(diapo => {
-        diapo.classList.remove('isActive')
-    })
+  diapos.forEach(diapo => {
+    diapo.classList.remove('isActive')
+  })
 }
-// Ajout du titre de la page en version mobile
-    const parentDivAjoute = document.querySelector('.container');
-    const titrePage = document.title;
-    const largeurFenetre = window.innerWidth;
-    const divAjoute = document.createElement('div');
-  
+
 // window.onload = ajoutDivMobile;
 // window.onresize = ajoutDivMobile;
-
-function ajoutDivMobile () {
-    if (largeurFenetre < 1100){
-        divAjoute.className = 'divTitreMobile';
-        divAjoute.innerHTML = titrePage;
-        parentDivAjoute.parentNode.insertBefore(divAjoute, parentDivAjoute);       
-    // } else { divAjoute.parentNode.removeChild(divAjoute); 
-    }
-}
