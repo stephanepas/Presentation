0e9dc81437397e7baaa722ce1a018de76b44caf6
stephanepas <stephane.pastor88@free.fr>
2022-01-17 09:44:56 +0100

    commit

--- app.js
+++ app.js
@@ -70,13 +70,16 @@ for (let i = 0; i < diapos.length; i++) {
   let diap = i + 1;
   const numDiapo = 'Diapo ' + diap + '/' + diapos.length;
   diapos[i].insertAdjacentHTML('afterbegin', numDiapo);
+  // Masque les diapos au dessus de la troisième
+  if (i > 2) {
+    diapos[i].style.display = "none";
+  }
   // agrandissement et affichage suivant le nombre de diapos
   diapos[i].addEventListener('click', () => {
     removeActiveClasses();
-    diapos[i].classList.add('isActive');
-    // s'il y a plus de 3 diapos
-    if (diapos.length > 3) {
-      if (i < 2) {
+    diapos[i].classList.add('isActive'); // ajoute la classe isActive sur la diapo cliquée
+    if (diapos.length > 3) { // effectué uniquement si plus de 3 diapos
+      if (i < 2) { // 3 premières diapos affichées
         for (let k = 0; k < diapos.length; k++) {
           if (k <= 2) {
             diapos[k].style.display = "block";
@@ -84,7 +87,7 @@ for (let i = 0; i < diapos.length; i++) {
             diapos[k].style.display = "none";
           }
         }
-      } else if (i < diapos.length - 1) {
+      } else if (i < diapos.length - 1) { // diapos intermédiaires = affichage 1 diapo avant et une diapo après
         for (let k = 0; k < diapos.length; k++) {
           if (k > i - 2 && k < i + 2) {
             diapos[k].style.display = "block";
@@ -92,9 +95,9 @@ for (let i = 0; i < diapos.length; i++) {
             diapos[k].style.display = "none";
           }
         }
-      } else {
+      } else { // dernière diapo  = affichage des 3 dernières diapos
         for (let k = 0; k < diapos.length; k++) {
-          if (k > diapos.length - 3) {
+          if (k > diapos.length - 4) {
             diapos[k].style.display = "block";
           } else {
             diapos[k].style.display = "none";
@@ -103,46 +106,46 @@ for (let i = 0; i < diapos.length; i++) {
       }
     }
   })
-  // Masque les diapos au dessus de la troisième
-  if (i > 2) {
-    diapos[i].style.display = "none";
-  }
 }
-
+// supprime la classe isActive sur toutes les diapos
 function removeActiveClasses() {
   diapos.forEach(diapo => {
     diapo.classList.remove('isActive')
   })
 }
-const titrePage = document.title;
-if (titrePage == "Présentation de la stratégie de webmarketing") {
-console.log(titrePage)
-// Animation des popup
-// Get the modal
-const modal = document.querySelector('.myModal');
-
-// Get the button that opens the modal
-const btn = document.querySelector('.myBtn');
-
-// Get the <span> element that closes the modal
-const span = document.querySelector('.close');
-// When the user clicks the button, open the modal
-console.log(modal)
-console.log(span)
-console.log(btn)
-btn.addEventListener('click', () => {
-  modal.style.display = "block";
-})
-
-// When the user clicks on <span> (x), close the modal
+// affichage des Popup
+const modal = document.querySelector('.myModal'); // récupére la fenêtre Popup
+const btn = document.querySelectorAll('.myBtn'); // récupère tous les liens texte souligné type bouton (classe myBtn)
+const span = document.querySelector('.close'); // récupère l'élément <span> qui ferme la Popup
+const textModal = document.querySelector('.textModal'); // récupère l'élément text de la Popup
+const titreModal = document.querySelector('.titreModal'); // récupère l'élément titre de la Popup
+// quand click sur un des liens myBtn, ouvre la fenêtre
+for (let b = 0; b < btn.length; b++) {
+  btn[b].addEventListener('click', () => { // quand clique sur un lien
+    titreModal.innerHTML = btn[b].textContent; // change le titre de la Popup
+    btn[b].style.color = "var(--tertiaire)"; // change la couleur du lien
+    if (b == 0) { // texte spécifique pour Solution
+      textModal.innerHTML = "à la place de Produits<br><br>Employeurs: service d'abonnement et accès à la CVthèque<br><br>Candidats: gratuit et accès aux offres";
+    } else if (b == 1) { // texte spécifique pour Accès
+      textModal.innerHTML = "au lieu de Place (emplacement)<br><br>Le service doit être accessible<br><br>n’importe où ...<br>n’importe quand ...<br>et ... sur n’importe quel appareil<br><br> Il faut développer une présence intégrée cross-canal qui considère le cycle d’activité du client";
+    } else if (b == 2) { // texte spécifique pour Valeur
+      textModal.innerHTML = "à la place de Price<br><br>Perception, valorisation du service<br><br>Accroître l'image de marque, pour cela le service doit être de qualité";
+    } else { // texte spécifique pour Education
+      textModal.innerHTML = "c'était la Promotion (communication)<br><br>Eduquer autour du handicap<br><br>Développer le contenu informatif";
+    }
+    modal.style.display = "block"; // affiche la fenêtre
+  })
+}
+// quand clique sur la croix <span> (x), ferme la fenêtre
 span.addEventListener('click', () => {
-  modal.style.display = "none";
+  fermePopUp(); // appel fonction
 })
-
-// When the user clicks anywhere outside of the modal, close it
+// quand clique sur le reste de la page, ferme la fenêtre
 document.addEventListener('click', (event) => {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == modal) { // Le cadre div.modal autour de la fenêtre est transparent mais fait 100% hauteur/largeur donc on ne peut que cliquer dessus sans le savoir !!
+    fermePopUp(); // appel fonction
   }
 })
+function fermePopUp() { // fonction ferme la fenêtre
+  modal.style.display = "none";
 }
